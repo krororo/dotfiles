1fe5bcf7e2e8a63f70d1f3a2ce38308d943fe02f
krororo <heavens.feel.7@gmail.com>
2016-01-01 17:10:19 +0900

    customize web-mode

--- .emacs.d/inits/20-web.el
+++ .emacs.d/inits/20-web.el
@@ -1,3 +1,7 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.vm\\'" . web-mode))
+
+(custom-set-variables
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2))
