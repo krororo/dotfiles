6fb672820b17c83711c78ff26a7011e03136f602
krororo <heavens.feel.7@gmail.com>
2016-05-11 09:02:12 +0900

    ignore jQuery, $

--- .emacs.d/inits/20-js2.el
+++ .emacs.d/inits/20-js2.el
@@ -1,6 +1,11 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+(add-hook 'js2-mode-hook
+          '(lambda()
+             (setq js2-global-externs (list "jQuery" "$"))
+             (setq js2-additional-externs (list "jQuery" "$"))))
+
 (custom-set-variables
  '(js-indent-level 2)
  '(js2-basic-offset 2))
