e40651c5eecdc95a1f22ecd32240e5b6be5162a0
krororo <heavens.feel.7@gmail.com>
2016-09-22 18:02:47 +0900

    flycheck: add keybind

--- .emacs.d/inits/20-flycheck.el
+++ .emacs.d/inits/20-flycheck.el
@@ -1,1 +1,5 @@
 (setq flycheck-check-syntax-automatically '(mode-enabled save))
+
+;; keybind
+(global-set-key (kbd "M-n") 'flycheck-next-error)
+(global-set-key (kbd "M-p") 'flycheck-previous-error)
