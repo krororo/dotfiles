0b2edd82df1d2c81e41d44db7587b6de8a074941
krororo <heavens.feel.7@gmail.com>
2016-09-23 09:14:08 +0900

    markdown table

--- .emacs.d/inits/20-markdown.el
+++ .emacs.d/inits/20-markdown.el
@@ -2,3 +2,12 @@
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(require 'org-table)
+(add-hook 'markdown-mode-hook 'orgtbl-mode)
+(defun cleanup-org-tables ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward "-+-" nil t) (replace-match "-|-"))))
+(add-hook 'markdown-mode-hook
+          #'(lambda()
+              (add-hook 'after-save-hook 'cleanup-org-tables  nil 'make-it-local)))
