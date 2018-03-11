366b936a36c3c7260dbbfa627153151b3ac43d9e
krororo <heavens.feel.7@gmail.com>
2018-03-12 08:34:39 +0900

    色々設定変更

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(flycheck-ruby-rubocop-executable nil)
+ '(magit-diff-highlight-hunk-body nil)
+ '(magit-log-margin
+   (quote
+    (t "%Y-%m-%d %H:%M:%S " magit-log-margin-width t 18)))
+ '(sh-indentation 4)
+ '(web-mode-attr-indent-offset 4)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-enable-auto-indentation nil)
+ '(web-mode-script-padding 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
