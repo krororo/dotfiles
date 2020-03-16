7235c053005d06c33392dc57165d0ba14b404905
krororo <krororo.07@gmail.com>
2020-03-16 10:25:24 +0900

    add mmm config

---
+++ .emacs.d/inits/20-mmm.el
@@ -0,0 +1,3 @@
+; see: https://qiita.com/akicho8/items/58c2ac5d762a2a4479c6
+(setq mmm-js-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))
+(setq mmm-typescript-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))
