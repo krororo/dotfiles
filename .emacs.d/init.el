9c9c7bae83ebf5920b8c414a67191d86cc2b5b98
krororo <krororo.07@gmail.com>
2020-04-01 15:24:46 +0900

    scratch バッファの保持を leaf で書き換え

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -137,31 +137,34 @@
     :el-get t
     :config (global-git-gutter-mode t))
 
-  ;; don't remove *scratch* buffer
-  (defun my-make-scratch (&optional arg)
-    (interactive)
-    (progn
-      ;; "*scratch*" を作成して buffer-list に放り込む
-      (set-buffer (get-buffer-create "*scratch*"))
-      (funcall initial-major-mode)
-      (erase-buffer)
-      (when (and initial-scratch-message (not inhibit-startup-message))
-        (insert initial-scratch-message))
-      (or arg (progn (setq arg 0)
-                     (switch-to-buffer "*scratch*")))
-      (cond ((= arg 0) (message "*scratch* is cleared up."))
-            ((= arg 1) (message "another *scratch* is created")))))
-  (add-hook 'kill-buffer-query-functions
-            ;; *scratch* バッファで kill-buffer したら内容を消去するだけにする
-            (lambda ()
-              (if (string= "*scratch*" (buffer-name))
-                  (progn (my-make-scratch 0) nil)
-                t)))
-  (add-hook 'after-save-hook
-            ;; *scratch* バッファの内容を保存したら *scratch* バッファを新しく作る
-            (lambda ()
-              (unless (member (get-buffer "*scratch*") (buffer-list))
-                (my-make-scratch 1)))))
+  (leaf *keep-scratch-buffer
+    :doc "don't remove *scratch* buffer"
+    :preface
+    (defun my-make-scratch (&optional arg)
+      (interactive)
+      (progn
+        ;; "*scratch*" を作成して buffer-list に放り込む
+        (set-buffer (get-buffer-create "*scratch*"))
+        (funcall initial-major-mode)
+        (erase-buffer)
+        (when (and initial-scratch-message (not inhibit-startup-message))
+          (insert initial-scratch-message))
+        (or arg (progn (setq arg 0)
+                       (switch-to-buffer "*scratch*")))
+        (cond ((= arg 0) (message "*scratch* is cleared up."))
+              ((= arg 1) (message "another *scratch* is created")))))
+    :hook
+    ((kill-buffer-query-functions
+      ;; *scratch* バッファで kill-buffer したら内容を消去するだけにする
+      . (lambda ()
+          (if (string= "*scratch*" (buffer-name))
+              (progn (my-make-scratch 0) nil)
+            t)))
+     (after-save-hook
+      ;; *scratch* バッファの内容を保存したら *scratch* バッファを新しく作る
+      . (lambda ()
+          (unless (member (get-buffer "*scratch*") (buffer-list))
+            (my-make-scratch 1)))))))
 
 ;; misc
 (el-get-bundle ag)
