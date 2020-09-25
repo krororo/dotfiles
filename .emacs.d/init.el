9d5d5217b0cc0fe6dec4fcc9f7431ac262e7a758
krororo <krororo.07@gmail.com>
2020-09-25 10:01:41 +0900

    emacs: proxy設定の処理を削除

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,17 +1,6 @@
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
-;; load proxy setting
-;; example
-;;  (setq url-proxy-services '(("http" . "proxy.example.com:8080")
-;;                            ("https" . "proxy.example.com:8080")))
-;;  (setq url-http-proxy-basic-auth-storage
-;;        (list (list "proxy.example.com:8080"
-;;                    (cons "description"
-;;                          (base64-encode-string "username:password")))))
-(when (file-exists-p (expand-file-name "~/.emacs.d/proxy-setting.el"))
-  (load-file (expand-file-name "~/.emacs.d/proxy-setting.el")))
-
 (add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
