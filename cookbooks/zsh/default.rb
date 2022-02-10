cc71672ce5f346d169813b74fd8a2eabd74dfddc
krororo <krororo.07@gmail.com>
2022-02-10 17:54:45 +0900

    mitamae: update zsh cookbook

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -3,6 +3,7 @@ dotfile '.zshrc'
 
 git 'zsh-autosuggestions' do
   repository 'https://github.com/zsh-users/zsh-autosuggestions'
-  destination File.expand_path('../../../config/.zsh/zsh-autosuggestions', __FILE__)
+  destination "#{ENV['HOME']}/.zsh/zsh-autosuggestions"
   user node[:user]
+  action :nothing
 end
