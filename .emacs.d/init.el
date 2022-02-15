05c01892b343a80700db70942ddc11f6cd2ea014
krororo <krororo.07@gmail.com>
2022-02-15 17:55:53 +0900

    emacs: set web-mode-block-padding

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -648,6 +648,7 @@ properly disable mozc-mode."
   :mode "\\.html?\\'" "\\.vm\\'" "\\.jsp\\'" "\\.vue\\'"
   :custom ((web-mode-enable-auto-indentation . nil)
            (web-mode-attr-indent-offset . 2)
+           (web-mode-block-padding . 2)
            (web-mode-code-indent-offset . 2)
            (web-mode-css-indent-offset . 2)
            (web-mode-markup-indent-offset . 2)
