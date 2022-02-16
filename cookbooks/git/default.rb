b45d58daca25cbc43702cd216fbdf119d5aed04d
krororo <krororo.07@gmail.com>
2022-02-16 09:46:30 +0900

    mitamae: refactor git

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -2,8 +2,9 @@ xdg_config 'git/config'
 xdg_config 'git/commit_template'
 
 github_release 'git-delta' do
+  v = '0.12.0'
   repository 'dandavison/delta'
-  version '0.12.0'
-  filename 'git-delta_0.12.0_amd64.deb'
-  not_if 'command -v delta'
+  version v
+  filename "git-delta_#{v}_amd64.deb"
+  not_if %(command -v delta && [ $(delta --version | egrep -o "([0-9]+.){2}[0-9]+") = #{v} ])
 end
