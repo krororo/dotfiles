48e2957d8f67c1b74729f58e75c9282fec33e768
krororo <krororo.07@gmail.com>
2022-04-20 15:51:38 +0900

    mitamae: add docker install

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -1,5 +1,30 @@
 dotfile '.docker'
 
+%w[ca-certificates curl gnupg lsb-release].each do |pkg|
+  package pkg
+end
+
+keyring_path = '/usr/share/keyrings/docker-archive-keyring.gpg'
+
+execute 'Download keyring' do
+  keyring_url = 'https://download.docker.com/linux/ubuntu/gpg'
+  command "curl -fsSL #{keyring_url} | sudo gpg --dearmor -o #{keyring_path}"
+  not_if "test -f #{keyring_path}"
+end
+
+file '/etc/apt/sources.list.d/docker.list' do
+  arch = run_command('dpkg --print-architecture').stdout.chomp
+  lsb_release = run_command('lsb_release -cs').stdout.chomp
+  content <<~CONTENT
+    deb [arch=#{arch} signed-by=#{keyring_path}] https://download.docker.com/linux/ubuntu #{lsb_release} stable
+  CONTENT
+  mode '644'
+end
+
+%w[docker-ce docker-ce-cli containerd.io].each do |pkg|
+  package pkg
+end
+
 cli_plugins_dir = "#{ENV['HOME']}/.docker/cli-plugins"
 directory cli_plugins_dir do
   owner node[:user]
