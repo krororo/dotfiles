08ac92dde574ba070fe54a78771f2b89ca450700
krororo <krororo.07@gmail.com>
2022-05-09 11:20:35 +0900

    mitamae: update docker compose to v2.5.0

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -31,7 +31,7 @@ directory cli_plugins_dir do
 end
 
 execute 'Download docker-compose' do
-  v = 'v2.4.1'
+  v = 'v2.5.0'
   url = "https://github.com/docker/compose/releases/download/#{v}/docker-compose-linux-x86_64"
 
   user node[:user]
