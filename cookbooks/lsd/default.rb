b20deb638b73d5376e31ed58e1cba2d69d43a686
krororo <krororo.07@gmail.com>
2022-06-13 09:42:30 +0900

    mitamae: add lsd

---
+++ cookbooks/lsd/default.rb
@@ -0,0 +1,7 @@
+github_release 'lsd' do
+  v = '0.22.0'
+  repository 'Peltoche/lsd'
+  version v
+  filename "lsd_#{v}_amd64.deb"
+  not_if %(command -v lsd && [ $(lsd --version | egrep -o "([0-9]+.){2}[0-9]+") = #{v} ])
+end
