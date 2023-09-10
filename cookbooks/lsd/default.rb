05cc910a8e70935de0b274364965465cd2421957
krororo <krororo.07@gmail.com>
2023-09-10 15:09:03 +0900

    mitamae: use lsd package

--- cookbooks/lsd/default.rb
+++
@@ -1,7 +0,0 @@
-github_release 'lsd' do
-  v = '0.23.1'
-  repository 'Peltoche/lsd'
-  version v
-  filename "lsd_#{v}_amd64.deb"
-  not_if %(command -v lsd && [ $(lsd --version | egrep -o "([0-9]+.){2}[0-9]+") = #{v} ])
-end
