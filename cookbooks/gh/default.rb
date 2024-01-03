7bf1dd0c1d3c2ce3fd745e5b8ffc8fab2f8cc4d7
krororo <krororo.07@gmail.com>
2024-01-03 23:25:02 +0900

    mitamae: gh setup move to base

--- cookbooks/gh/default.rb
+++ cookbooks/gh/default.rb
@@ -1,21 +1,23 @@
-execute 'Download keyring' do
-  keyring_url = 'https://cli.github.com/packages/githubcli-archive-keyring.gpg'
-  keyring_path = '/usr/share/keyrings/githubcli-archive-keyring.gpg'
-  command "curl -fsSL #{keyring_url} | sudo dd of=#{keyring_path}"
-  not_if "test -f #{keyring_path}"
-end
+if node[:platform] != 'darwin'
+  execute 'Download keyring' do
+    keyring_url = 'https://cli.github.com/packages/githubcli-archive-keyring.gpg'
+    keyring_path = '/usr/share/keyrings/githubcli-archive-keyring.gpg'
+    command "curl -fsSL #{keyring_url} | sudo dd of=#{keyring_path}"
+    not_if "test -f #{keyring_path}"
+  end
 
-execute 'sudo apt update' do
-  subscribes :run, 'execute[Add apt source]', :immediately
-  action :nothing
-end
+  execute 'sudo apt update' do
+    subscribes :run, 'execute[Add apt source]', :immediately
+    action :nothing
+  end
 
-execute 'Add apt source' do
-  command 'echo "deb [arch=$(dpkg --print-architecture) ' \
-          'signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] ' \
-          'https://cli.github.com/packages stable main" ' \
-          '| sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null'
-  not_if "test -f /etc/apt/sources.list.d/github-cli.list"
+  execute 'Add apt source' do
+    command 'echo "deb [arch=$(dpkg --print-architecture) ' \
+            'signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] ' \
+            'https://cli.github.com/packages stable main" ' \
+            '| sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null'
+    not_if "test -f /etc/apt/sources.list.d/github-cli.list"
+  end
 end
 
 package 'gh'
--- recipes/base/default.rb
+++ recipes/base/default.rb
@@ -8,5 +8,6 @@ xdg_config 'ov/config.yaml'
 package 'lsd'
 
 include_recipe '../../cookbooks/emacs'
+include_recipe '../../cookbooks/gh'
 include_recipe '../../cookbooks/git'
 include_recipe '../../cookbooks/zsh'
