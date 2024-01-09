11fb34b09ba4f0c920b6ed5ae7b2852cb8fa5e2e
krororo <krororo.07@gmail.com>
2024-01-09 17:26:43 +0900

    mitamae: add some packages for darwin

--- recipes/darwin/default.rb
+++ recipes/darwin/default.rb
@@ -1,4 +1,12 @@
+package 'coreutils'
+package 'findutils'
+package 'grep'
 package 'noborus/tap/ov'
 package 'peco'
+
+cask 'alt-tab'
+cask 'iterm2'
+# cask 'karabiner-elements' # TBD
+cask 'raycast'
 
 xdg_config 'karabiner/assets/complex_modifications/my_emacs.json'
