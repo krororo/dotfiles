d622b5448555b937cff53ec017d1b051d17a1b66
krororo <krororo.07@gmail.com>
2024-01-09 09:43:49 +0900

    mitamae: zsh-autosuggestions use git

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -3,13 +3,9 @@ dotfile '.zshrc'
 
 package 'zsh-syntax-highlighting'
 
-if node[:platform] == 'darwin'
-  package 'zsh-autosuggestions'
-else
-  git 'zsh-autosuggestions' do
-    repository 'https://github.com/zsh-users/zsh-autosuggestions'
-    destination "#{ENV['HOME']}/.zsh/zsh-autosuggestions"
-    user node[:user]
-    action :nothing
-  end
+git 'zsh-autosuggestions' do
+  repository 'https://github.com/zsh-users/zsh-autosuggestions'
+  destination "#{ENV['HOME']}/.zsh/zsh-autosuggestions"
+  user node[:user]
+  action :nothing
 end
