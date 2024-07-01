a9cce7beb54ab0ca8026add321a79b915952f298
krororo <krororo.07@gmail.com>
2024-07-01 10:23:10 +0900

    mitamae: add bat

--- recipes/base/default.rb
+++ recipes/base/default.rb
@@ -1,10 +1,12 @@
 dotfile '.vimrc'
 
+xdg_config 'bat/config'
 xdg_config 'gem/gemrc'
 xdg_config 'irb/irbrc'
 xdg_config 'lsd/config.yaml'
 xdg_config 'ov/config.yaml'
 
+package 'bat'
 package 'fzf'
 package 'lsd'
 package 'jq'
