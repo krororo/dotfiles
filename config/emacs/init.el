bc73e0bd4a484699d890b4a4cc2eafaba5137def
krororo <krororo.07@gmail.com>
2024-08-06 09:11:50 +0900

    emacs: add yaml-mode-hook to eglot

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -579,7 +579,7 @@ properly disable mozc-mode."
 
 (leaf eglot
   :ensure t
-  :hook ((ruby-mode-hook typescript-mode-hook) . eglot-ensure)
+  :hook ((ruby-mode-hook typescript-mode-hook yaml-mode-hook) . eglot-ensure)
   :advice
   (:around save-buffers-kill-emacs
            (lambda (orig-fun &rest args)
