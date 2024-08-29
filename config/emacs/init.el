9cfc00fe749701ad9716e72507b3820b974edeb3
krororo <krororo.07@gmail.com>
2024-08-29 11:43:24 +0900

    emacs: set to eglot-ignored-server-capabilities

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -586,7 +586,8 @@ properly disable mozc-mode."
              (cl-letf (((symbol-function #'process-list) (lambda ())))
                (apply orig-fun args))))
   :custom ((eglot-autoshutdown . t)
-           (eldoc-echo-area-use-multiline-p . nil))
+           (eldoc-echo-area-use-multiline-p . nil)
+           (eglot-ignored-server-capabilities . '(:documentOnTypeFormattingProvider)))
   :config
   (add-to-list 'eglot-server-programs '((ruby-mode ruby-ts-mode) "ruby-lsp")))
 
