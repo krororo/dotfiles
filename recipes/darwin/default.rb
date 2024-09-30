86e47faf1f554704e8c45f189ce2c0d17a07254d
krororo <krororo.07@gmail.com>
2024-09-30 11:37:37 +0900

    mitamae: add font-hackgen, font-hackgen-nerd

--- recipes/darwin/default.rb
+++ recipes/darwin/default.rb
@@ -8,6 +8,8 @@ package 'watch'
 cask 'alt-tab'
 cask 'deepl'
 cask 'devtoys'
+cask "font-hackgen"
+cask "font-hackgen-nerd"
 cask 'font-noto-color-emoji'
 cask 'iterm2'
 # cask 'karabiner-elements' # TBD
