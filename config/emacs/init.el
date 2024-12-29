1b2cb88c80d2393ddbd3b6919074dc845fdd0869
krororo <krororo.07@gmail.com>
2024-12-29 23:56:18 +0900

    emacs: remove dired-subtree

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -436,12 +436,6 @@ properly disable mozc-mode."
   (define-key dired-mode-map "a" 'dired-find-file)
   (define-key dired-mode-map (kbd "RET") 'dired-open-in-accordance-with-situation))
 
-(leaf dired-subtree
-  :ensure t
-  :bind ((:dired-mode-map
-          ("i" . dired-subtree-insert)
-          ("<tab>" . dired-subtree-remove))))
-
 (leaf vertico
   :ensure t
   :require vertico-directory
