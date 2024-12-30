80c0c25102fce7fc1bfe6139650e59a18f20715b
krororo <krororo.07@gmail.com>
2024-12-30 10:28:23 +0900

    mitamae: update xremap to 0.10.3

--- cookbooks/xremap/default.rb
+++ cookbooks/xremap/default.rb
@@ -2,7 +2,7 @@ xdg_config 'xremap/config.yml'
 
 github_release 'xremap' do
   repository 'xremap/xremap'
-  v = '0.10.2'
+  v = '0.10.3'
   version "v#{v}"
   filename 'xremap-linux-x86_64-x11.zip'
   bin_path = "#{ENV['HOME']}/bin/xremap"
