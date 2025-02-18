4a6f51ff132e37f601a34fb0b862272b0a404a9f
krororo <krororo.07@gmail.com>
2025-02-18 15:35:13 +0900

    emacs: update copilot config

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -949,6 +949,8 @@ properly disable mozc-mode."
   :custom
   (copilot-indent-offset-warning-disable . t)
   (copilot-max-char . -1)
+  (copilot-server-log-level . 3)
+  (copilot-version . "1.272.0")
   :custom-face
   (copilot-overlay-face . '((t (:background "gray5")))))
 
