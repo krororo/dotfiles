32211121b258932f5b8819d23fe3e846591b3583
krororo <krororo.07@gmail.com>
2025-03-29 17:06:10 +0900

    mitamae: remove action nothing from zsh-autosuggestions

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -7,5 +7,4 @@ git 'zsh-autosuggestions' do
   repository 'https://github.com/zsh-users/zsh-autosuggestions'
   destination "#{ENV['HOME']}/.zsh/zsh-autosuggestions"
   user node[:user]
-  action :nothing
 end
