bc23e4b036e8d0abd47d71eaab51e572f7a30d1e
krororo <krororo.07@gmail.com>
2025-04-05 18:50:37 +0900

    mitamae: add chsh for zsh

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -8,3 +8,9 @@ git 'zsh-autosuggestions' do
   destination "#{ENV['HOME']}/.zsh/zsh-autosuggestions"
   user node[:user]
 end
+
+if node[:platform] != "darwin"
+  execute "chsh -s /usr/bin/zsh #{node[:user]}" do
+    not_if "grep -qP '#{node[:user]}.+/usr/bin/zsh' /etc/passwd"
+  end
+end
