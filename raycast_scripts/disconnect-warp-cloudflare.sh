ff9711d4ecc8a96b5ad7a0baaabcc4b42bb41285
krororo <krororo.07@gmail.com>
2025-04-17 09:49:36 +0900

    raycast: add scripts

---
+++ raycast_scripts/disconnect-warp-cloudflare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Disconnect Warp Cloudflare
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+warp-cli disconnect
