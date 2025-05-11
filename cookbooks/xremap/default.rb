b13fc3e33ed8c1cfd826e54bb3296e1ff037e6f7
krororo <krororo.07@gmail.com>
2025-05-11 15:28:22 +0900

    mitamae: update xremap to 0.10.12

--- cookbooks/xremap/default.rb
+++ cookbooks/xremap/default.rb
@@ -2,7 +2,7 @@ xdg_config 'xremap/config.yml'
 
 github_release 'xremap' do
   repository 'xremap/xremap'
-  v = "0.10.11"
+  v = "0.10.12"
   version "v#{v}"
   filename 'xremap-linux-x86_64-x11.zip'
   bin_path = "#{ENV['HOME']}/bin/xremap"
