d4981e50216f4bc07735db8d35cf3e9dd31e9557
krororo <krororo.07@gmail.com>
2025-08-23 16:09:53 +0900

    mitamae: package docker-compose-plugin

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -26,29 +26,15 @@ file '/etc/apt/sources.list.d/docker.list' do
   mode '644'
 end
 
-%w[docker-ce docker-ce-cli containerd.io].each do |pkg|
+%w[
+  docker-ce
+  docker-ce-cli
+  docker-compose-plugin
+  containerd.io
+].each do |pkg|
   package pkg
 end
 
 execute "gpasswd -a #{node[:user]} docker" do
   not_if { run_command("id #{node[:user]}").stdout.include?('docker') }
-end
-
-cli_plugins_dir = "#{ENV['HOME']}/.docker/cli-plugins"
-directory cli_plugins_dir do
-  user node[:user]
-end
-
-execute 'Download docker-compose' do
-  v = 'v2.29.1'
-  url = "https://github.com/docker/compose/releases/download/#{v}/docker-compose-linux-x86_64"
-
-  user node[:user]
-  command "curl -sL -o #{cli_plugins_dir}/docker-compose #{url} && chmod +x #{cli_plugins_dir}/docker-compose"
-  only_if do
-    next true unless File.exist?("#{cli_plugins_dir}/docker-compose")
-
-    m = run_command('docker compose version').stdout.match(/v(\d+\.){2}\d+/)
-    m && m[0] != v
-  end
 end
