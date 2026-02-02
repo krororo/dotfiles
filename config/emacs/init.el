96abbe5e571c7acefb48c862d8fb705dae323489
krororo <krororo.07@gmail.com>
2026-02-02 09:30:53 +0900

    emacs: Remove global-comment-translate-mode auto-enable

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1081,7 +1081,6 @@ properly disable mozc-mode."
 (leaf comment-translate
   :vc (:url "https://github.com/Kyure-A/emacs-comment-translate")
   :require t
-  :global-minor-mode global-comment-translate-mode
   :custom
   (comment-translate-target-language . "ja")
   (comment-translate-hover-source . 'mouse))
