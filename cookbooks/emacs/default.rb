0aff944240bc88e4c4b626ca4a0f2e6237a3d8c1
krororo <krororo.07@gmail.com>
2026-02-02 09:28:42 +0900

    mise: Switch to cask installation of Emacs for macOS
    Replace manual brew install with emacs-plus-app cask to simplify
    installation and remove manual symlink creation. This streamlines
    the macOS Emacs setup process.

--- cookbooks/emacs/default.rb
+++ cookbooks/emacs/default.rb
@@ -1,15 +1,6 @@
 if node[:platform] == 'darwin'
   brew_tap "d12frosted/emacs-plus"
-  execute "brew install emacs-plus --with-xwidgets --with-imagemagick --with-modern-pen-icon" do
-    # Optimized `brew list` command
-    # ref. https://github.com/mizzy/specinfra/blob/v2.92.0/lib/specinfra/command/darwin/base/package.rb#L46
-    not_if "ls -1 $(brew --prefix)/Cellar/ | grep ^emacs-plus"
-  end
-
-  link "/Applications/Emacs.app" do
-    to "/opt/homebrew/opt/emacs-plus/Emacs.app"
-    force true
-  end
+  cask "emacs-plus-app"
 else
   package 'emacs'
   package "emacs-mozc"
